5cd0fbaf5cfaf98c0026183b8caae5340b579927
Casper Hoegalmen <Church16413@gmail.com>
2021-03-12 00:08:55 +0100

    World Coordinate Gaze Movement

--- BreakoutGame/Assets/Scripts/RacketMovement.cs
+++ BreakoutGame/Assets/Scripts/RacketMovement.cs
@@ -6,28 +6,44 @@ using Tobii.Gaming;
 public class RacketMovement : MonoBehaviour
 {
     public float speed = 150;
-    public int Input;
+    public int Move_Input;
+    public Camera mainCam;
+    public float Racket_Screen_Pos;
+    public int Dead_Space;
     // Update is called once per frame
     void FixedUpdate()
-    {   
-//        float horizontalInput = Input.GetAxisRaw("Horizontal");
-        GetComponent<Rigidbody2D>().velocity = Vector2.right * Input * speed;
+    {
+        Print_Input();
+        Racket_Screen_Pos = mainCam.WorldToScreenPoint(this.gameObject.transform.TransformPoint(this.gameObject.transform.position)).x;
+        Gaze_Racket_Input();
+        //        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * Move_Input * speed;
     }
 
     void Gaze_Racket_Input()
     {
-        if(TobiiAPI.GetGazePoint().Screen.x < this.gameObject.transform.position.x)
+        if (TobiiAPI.GetGazePoint().Screen.x < Racket_Screen_Pos && Racket_Screen_Pos - TobiiAPI.GetGazePoint().Screen.x > Dead_Space)
         {
-            Input = -1;
+            Move_Input = -1;
         }
-        else if (TobiiAPI.GetGazePoint().Screen.x > this.gameObject.transform.position.x)
+        else if (TobiiAPI.GetGazePoint().Screen.x > Racket_Screen_Pos && TobiiAPI.GetGazePoint().Screen.x - Racket_Screen_Pos > Dead_Space)
         {
-            Input = 1;
+            Move_Input = 1;
         }
         else
         {
-            Input = 0;
+            Move_Input = 0;
         }
-
     }
+
+    void Print_Input()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Racket_Screen_Pos: " + Racket_Screen_Pos);
+            Debug.Log("Gaze Point: " + TobiiAPI.GetGazePoint().Screen.x);
+
+        }
+    }
+
 }
